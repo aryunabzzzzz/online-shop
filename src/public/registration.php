7b6bef77ab980bf5d39faaf24c3a4845e437a22c
aryunabzzzzz <a11ab9@yandex.ru>
2024-02-24 19:10:16 +0700

    redesigned registrarion page

--- src/public/registration.php
+++ src/public/registration.php
@@ -1,84 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<body>
 <form action="post_registration.php" method="POST">
-    <div class="container">
-        <h1>Registration</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr>
 
-        <label for="name"><b>Name</b></label>
-        <input type="text" placeholder="Enter Name" name="name" id="name" required>
+    <input id="signup" name="action" type="radio" value="signup">
+    <label for="signup">Sign up</label>
 
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" id="email" required>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-        <hr>
-
-        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-        <button type="submit" class="registerbtn">Register</button>
+    <div id="wrapper">
+        <div id="arrow"></div>
+        <input id="name" placeholder="Name" type="text" name="name">
+        <input id="email" placeholder="Email" type="text" name="email">
+        <input id="psw" placeholder="Password" type="password" name="psw">
+        <input id="psw-repeat" placeholder="Repeat password" type="password" name="psw-repeat">
     </div>
+    <button type="submit">
+    <span>
+      Sign up
+    </span>
+    </button>
 
-    <div class="container signin">
-        <p>Already have an account? <a href="#">Sign in</a>.</p>
-    </div>
 </form>
 
-<style>
-    * {box-sizing: border-box}
+<div id="hint">Already have an account? <a href="http://localhost:8080/singin.php">Sign in</div>
 
-    /* Add padding to containers */
-    .container {
-        padding: 16px;
-    }
+</body>
+</html>
 
-    /* Full-width input fields */
-    input[type=text], input[type=password] {
-        width: 100%;
-        padding: 15px;
-        margin: 5px 0 22px 0;
-        display: inline-block;
-        border: none;
-        background: #f1f1f1;
-    }
-
-    input[type=text]:focus, input[type=password]:focus {
-        background-color: #ddd;
-        outline: none;
-    }
-
-    /* Overwrite default styles of hr */
-    hr {
-        border: 1px solid #f1f1f1;
-        margin-bottom: 25px;
-    }
-
-    /* Set a style for the submit/register button */
-    .registerbtn {
-        background-color: #04AA6D;
-        color: white;
-        padding: 16px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-        opacity: 0.9;
-    }
-
-    .registerbtn:hover {
-        opacity:1;
-    }
-
-    /* Add a blue text color to links */
-    a {
-        color: dodgerblue;
-    }
-
-    /* Set a grey background color and center the text of the "sign in" section */
-    .signin {
-        background-color: #f1f1f1;
-        text-align: center;
-    }
-</style>
+<?php
+require_once ('./style.php');
