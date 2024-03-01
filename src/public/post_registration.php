37c2392dad4089df1e5d0fe152412b99ca98e10a
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-01 23:11:42 +0700

    Оптимизирован код

--- src/public/post_registration.php
+++ src/public/post_registration.php
@@ -1,75 +1,70 @@
 <?php
 //процедура валидации
-function validation(array $arr):array
+function validateRegistration(array $arr):array
 {
     $errors = [];
 
-    if (isset($arr['name'])){
-        $name = $arr['name'];
-        if (empty($name)){
-            $errors['name'] = 'Поле не должно быть пустым';
-        } elseif (strlen($name)<3){
-            $errors['name'] = 'Слишком короткое имя пользователя';
-        }
-    } else {
+    if (empty($arr['name'])){
         $errors['name'] = 'Поле не должно быть пустым';
-    }
-
-    if (isset($arr['email'])){
-        $email = $arr['email'];
-        if (empty($email)){
-            $errors['email'] = 'Поле не должно быть пустым';
-        } elseif(stristr($email, '@') === FALSE) {
-            $errors['email'] = 'Некорректно введён email';
-        }
     } else {
-        $errors['email'] = 'Поле не должно быть пустым';
+        $name = $arr['name'];
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            $errors['name'] = "Поле должно содержать только буквы и пробелы";
+        }
     }
 
-    if (isset($arr['psw'])){
+    if (empty($arr['email'])){
+        $errors['email'] = 'Поле не должно быть пустым';
+    } else {
+        $email = $arr['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Некорректно введён email";
+        } else {
+            $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
+            $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+            $stmt->execute(['email'=>$email]);
+            $user = $stmt->fetch();
+
+            if ($user){
+                $errors['email'] = 'Пользователь с таким адресом почты уже существует';
+            }
+        }
+    }
+
+    if (empty($arr['psw'])) {
+        $errors['psw'] = 'Поле не должно быть пустым';
+    } else {
         $password = $arr['psw'];
-        if (empty($password)){
-            $errors['psw'] = 'Поле не должно быть пустым';
-        } elseif (strlen($password)<3){
+        if (strlen($password)<3){
             $errors['psw'] = 'Слишком короткий пароль';
         }
-    } else {
-        $errors['psw'] = 'Поле не должно быть пустым';
     }
 
-    if (isset($arr['psw-repeat'])){
+    if (empty($arr['psw-repeat'])) {
+        $errors['psw-repeat'] = 'Поле не должно быть пустым';
+    } else {
         $passwordRepeat = $arr['psw-repeat'];
-        if (empty($passwordRepeat)){
-            $errors['psw-repeat'] = 'Поле не должно быть пустым';
-        }
-        if ($passwordRepeat != $password){
+        if ($passwordRepeat !== $password){
             $errors['psw-repeat'] = 'Пароли не совпадают!';
         }
-    } else {
-        $errors['psw-repeat'] = 'Поле не должно быть пустым';
     }
-//проверка на повторяющуюся почту
-    $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $stmt->execute(['email'=>$email]);
-    $user = $stmt->fetch();
 
-    if ($user){
-        $errors['email'] = 'Пользователь с таким адресом почты уже существует';
-    }
     return $errors;
 }
 
-$errors = validation($_POST);
+$errors = validateRegistration($_POST);
 
 if (empty($errors)){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['psw'];
-    //создание объекта класса
-    $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
+
     //защита пароля
     $password = password_hash($password, PASSWORD_DEFAULT);
+
+    //создание объекта класса
+    $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
+
     //добавление данных пользователя в БД + защита от SQL-инъекции
     $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
     $stmt->execute(['name'=>$name, 'email'=>$email, 'password'=>$password]);
