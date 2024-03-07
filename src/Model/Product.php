c4276494e4c1153ff9f8db3f99748a8cc5109603
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-07 16:10:51 +0700

    add parent class Model

--- src/Model/Product.php
+++ src/Model/Product.php
@@ -1,11 +1,10 @@
 <?php
 
-class Product
+class Product extends Model
 {
     public function getAll()
     {
-        $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
-        $stmt = $pdo->query("SELECT * FROM products");
+        $stmt = $this->pdo->query("SELECT * FROM products");
         $products = $stmt->fetchAll();
 
         return $products;
--- src/Model/User.php
+++ src/Model/User.php
@@ -1,11 +1,10 @@
 <?php
 
-class User
+class User extends Model
 {
     public function getOneByEmail($email)
     {
-        $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email = :email");
         $stmt->execute(['email'=>$email]);
         $user = $stmt->fetch();
 
@@ -14,8 +13,7 @@ class User
 
     public function create($name, $email, $password)
     {
-        $pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
-        $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
         $stmt->execute(['name'=>$name, 'email'=>$email, 'password'=>$password]);
     }
 }
