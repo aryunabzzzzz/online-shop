1ad58f853affe096243838adbdf14eda1e464ff1
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-12 17:11:21 +0700

    refactoring app.php

--- src/Core/App.php
+++ src/Core/App.php
@@ -2,54 +2,66 @@
 
 class App
 {
+    private array $routes = [
+        '/registration'=>[
+            'GET'=>[
+                'class'=>'UserController',
+                'method'=>'getRegistration'
+            ],
+            'POST'=>[
+                'class'=>'UserController',
+                'method'=>'postRegistration'
+            ],
+        ],
+        '/login'=>[
+            'GET'=>[
+                'class'=>'UserController',
+                'method'=>'getLogin'
+            ],
+            'POST'=>[
+                'class'=>'UserController',
+                'method'=>'postLogin'
+            ],
+        ],
+        '/main'=>[
+            'GET'=>[
+                'class'=>'MainController',
+                'method'=>'getMain'
+            ],
+        ],
+        '/add_product'=>[
+            'POST'=>[
+                'class'=>'ProductController',
+                'method'=>'postAddProduct'
+            ],
+        ],
+        '/delete_product'=>[
+            'POST'=>[
+                'class'=>'ProductController',
+                'method'=>'postDeleteProduct'
+            ],
+        ],
+        '/cart'=>[
+            'GET'=>[
+                'class'=>'CartController',
+                'method'=>'getCart'
+            ],
+        ],
+    ];
     public function run(): void
     {
         $uri = $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
+        $routes = $this->routes;
 
-        if ($uri === '/registration'){
-            $obj = new UserController();
-            if ($method === 'GET'){
-                $obj->getRegistration();
-            } elseif ($method === 'POST'){
-                $obj->postRegistration();
-            } else {
-                echo "$method не поддерживается $uri";
-            }
-        } elseif ($uri === '/login'){
-            $obj = new UserController();
-            if ($method === 'GET'){
-                $obj->getLogin();
-            } elseif ($method === 'POST'){
-                $obj->postLogin();
-            } else {
-                echo "$method не поддерживается $uri";
-            }
-        } elseif ($uri === '/main'){
-            $obj = new MainController();
-            if($method === 'GET'){
-                $obj->getMain();
-            } else {
-                echo "$method не поддерживается $uri";
-            }
-        } elseif ($uri === '/add_product'){
-            $obj = new ProductController();
-            if ($method === 'POST'){
-                $obj->postAddProduct();
-            } else {
-                echo "$method не поддерживается $uri";
-            }
-        } elseif ($uri === '/delete_product'){
-            $obj = new ProductController();
-            if ($method === 'POST'){
-                $obj->postDeleteProduct();
-            } else {
-                echo "$method не поддерживается $uri";
-            }
-        } elseif ($uri === '/cart'){
-            $obj = new CartController();
-            if ($method === 'GET'){
-                $obj->getCart();
+        if (isset($routes[$uri])){
+            $routeMethod = $this->routes[$uri];
+            if (isset($routeMethod[$method])){
+                $test = $routeMethod[$method];
+                $className = $test['class'];
+                $function = $test['method'];
+                $obj = new $className;
+                $obj->$function();
             } else {
                 echo "$method не поддерживается $uri";
             }
