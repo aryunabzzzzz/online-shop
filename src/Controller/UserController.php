7d6a53e571eb1a017eaa8cb2678120e85c171989
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-12 18:57:28 +0700

    change logout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -143,15 +143,9 @@ class UserController
     public function getLogout(): void
     {
         session_start();
-        $_SESSION = array();
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params["path"], $params["domain"],
-                $params["secure"], $params["httponly"]
-            );
+        if (session_status() === PHP_SESSION_ACTIVE){
+            session_destroy();
         }
-        session_destroy();
         header('Location: /login');
     }
 
