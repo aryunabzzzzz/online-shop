44933a93f31f79124e396a2e8ba733ce42f02590
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-18 18:25:53 +0700

    добавлен тип данных для функции

--- src/Model/OrderProduct.php
+++ src/Model/OrderProduct.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class OrderProduct extends Model
 {
-    public function create(string $order_id, int $product_id,  int $quantity, float $price)
+    public function create(string $order_id, int $product_id,  int $quantity, float $price): void
     {
         $stmt = $this->pdo->prepare("INSERT INTO orders_products (order_id, product_id, quantity, price) VALUES (:order_id, :product_id, :quantity, :price)");
         $stmt->execute(['order_id'=>$order_id, 'product_id'=>$product_id, 'quantity'=>$quantity, 'price'=>$price]);
