479520932a1be88d722d55bfda99168b8bad750d
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-25 14:16:17 +0700

    исправлен тип данных product_id

--- src/Repository/OrderProductRepository.php
+++ src/Repository/OrderProductRepository.php
@@ -4,7 +4,7 @@ namespace Repository;
 
 class OrderProductRepository extends Repository
 {
-    public function create(string $order_id, int $product_id,  int $quantity, float $price): void
+    public function create(int $order_id, int $product_id,  int $quantity, float $price): void
     {
         $stmt = $this->pdo->prepare("INSERT INTO orders_products (order_id, product_id, quantity, price) VALUES (:order_id, :product_id, :quantity, :price)");
         $stmt->execute(['order_id'=>$order_id, 'product_id'=>$product_id, 'quantity'=>$quantity, 'price'=>$price]);
