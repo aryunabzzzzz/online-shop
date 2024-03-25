61c431a3ce2af4e55680c174cdef2d2287b46049
aryunabzzzzz <a11ab9@yandex.ru>
2024-03-25 14:18:01 +0700

    добавлен try/catch, не работает

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -12,4 +12,19 @@ class Repository
     {
         $this->pdo = new PDO("pgsql:host=db; port=5432; dbname=laravel","root", "root");
     }
+
+    public function beginTransaction(): bool
+    {
+        return $this->pdo->beginTransaction();
+    }
+
+    public function rollbackTransaction(): bool
+    {
+        return $this->pdo->rollBack();
+    }
+
+    public function commitTransaction(): bool
+    {
+        return $this->pdo->commit();
+    }
 }
--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -5,30 +5,43 @@ namespace Service;
 use Repository\OrderRepository;
 use Repository\OrderProductRepository;
 use Repository\UserProductRepository;
+use Repository\Repository;
 
 class OrderService
 {
     private OrderRepository $orderRepository;
     private OrderProductRepository $orderProductRepository;
     private UserProductRepository $userProductRepository;
+    private Repository $repository;
 
     public function __construct()
     {
         $this->orderRepository = new OrderRepository();
         $this->orderProductRepository = new OrderProductRepository();
         $this->userProductRepository = new UserProductRepository();
+        $this->repository = new Repository();
     }
 
     public function create(int $userId, string $fullName, string $phone, string $address): void
     {
-        $this->orderRepository->create($userId, $fullName, $phone, $address);
-        $orderId = $this->orderRepository->getOrderId();
-        $cartProducts = $this->userProductRepository->getAllByUserId($userId);
+        $this->repository->beginTransaction();
 
-        foreach ($cartProducts as $cartProduct) {
-            $this->orderProductRepository->create($orderId, $cartProduct->getProductEntity()->getId(), $cartProduct->getQuantity(), $cartProduct->getProductEntity()->getPrice());
+        try {
+
+            $this->orderRepository->create($userId, $fullName, $phone, $address);
+            $orderId = $this->orderRepository->getOrderId();
+            $cartProducts = $this->userProductRepository->getAllByUserId($userId);
+
+            foreach ($cartProducts as $cartProduct) {
+                $this->orderProductRepository->create($orderId, $cartProduct->getProductEntity()->getId(), $cartProduct->getQuantity(), $cartProduct->getProductEntity()->getPrice());
+            }
+
+            $this->userProductRepository->deleteAllByUserId($userId);
+
+            $this->repository->commitTransaction();
+
+        } catch (\Throwable $exception){
+            $this->repository->rollbackTransaction();
         }
-
-        $this->userProductRepository->deleteAllByUserId($userId);
     }
 }
